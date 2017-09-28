34279e1d9bcec5975f8f415df037caec5a67c0c3
abreze <abdrzg163gmail.com>
2017-09-28 20:27:39 +0200

    ajout d'un fichier

---
+++ essai/exo1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int maximum(int a,int b){
+	if (a>b)
+	return a;
+	else return b;
+	}
+int main(){
+
+int a=5; int b=10;	
+printf("le max est :%d",maximum(a,b));
+	return 0;}
